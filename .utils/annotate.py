48040267bd4636a61848369bc5b9868f68c5f2af
Daniel Donadon <daniel.donadon@unesp.br>
2024-11-26 21:27:47 -0300

    chore: corrections to license tool

--- .utils/annotate.py
+++ .utils/annotate.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 
+# utb: UVa Online Judge toolbox
+# Copyright (C) 2024  Daniel Donadon
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
+import re
 import sys
 from pathlib import Path
 
@@ -13,35 +30,39 @@ def load(filename):
         return stream.readlines()
 
 
-def annotate(header, notice, filename, update):
+def annotate(header, notice, filename):
     content = load(filename)
-    start = 0, existing = None
+    start, existing = 0, None
     for index, line in enumerate(content):
         if index == start and line.startswith('#!'):
             start = index + 1
             continue
         if line == notice[0]:
-            existing = line
+            existing = index
             break
     if existing is not None:
-        if not update:
-            return
-        del content[existing - len(header):existing + len(notice)]
+        start = existing - len(header)
+        del content[start:existing + len(notice)]
     content[start:start] = header + notice
     with open(filename, 'w') as stream:
         stream.write(''.join(content))
     print(filename)
 
 
-def annotate_files(header, notice, files, update):
+def annotate_files(header, notice, files):
     for file in files:
-        annotate(header, notice, file, update)
+        annotate(header, notice, file)
 
 
-def annotate_python(header, notice, path, update):
-    header = ['# %s' % line for line in header]
-    notice = ['# %s' % line for line in notice]
-    annotate_files(header, notice, path.rglob('*.py'), update)
+def clean(line):
+    return re.sub(r'\s+\n$', '\n', line)
+
+
+def annotate_python(header, notice, path):
+    header.append('\n')
+    header = [clean(f'# { line }') for line in header]
+    notice = [clean(f'# { line }') for line in notice]
+    annotate_files(header, notice, path.rglob('*.py'))
 
 
 if __name__ == '__main__':
@@ -49,6 +70,6 @@ if __name__ == '__main__':
         print('Usage:  %s  directory  [header]  [notice]' % sys.argv[0])
         sys.exit(2)
     path = Path(sys.argv[1])
-    header = load(sys.argv[2] if len(sys.argv) >= 2 else 'copyright-header')
-    notice = load(sys.argv[3] if len(sys.argv) >= 3 else 'copyright-notice')
-    annotate_python(header, notice, path, False)
+    header = load(sys.argv[2] if len(sys.argv) > 2 else 'copyright-header')
+    notice = load(sys.argv[3] if len(sys.argv) > 3 else 'copyright-notice')
+    annotate_python(header, notice, path)
